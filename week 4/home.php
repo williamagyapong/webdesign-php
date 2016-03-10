14b9bcdbd4a72649df7fd0b21bbf6fa5f7b945ba
William Agyapong <willisco@github.com>
2016-03-10 02:16:16 +0000

    Posted week 4 tutorials files

---
+++ week 4/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body >
+
+<div style="background-image: url(images/progback2.jpg); background-repeat:repeat;  width: 100%; height: 750px"></div>
+
+ <div class="footer">
+      All rights reserved<br>
+      &copy 2016<br>
+      Agyapong Ofosu William<br>
+      BSc. Actuarial Science, KNUST.
+   </div>
+ 
+</body>
+</html>
---
+++ week 4/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+
+     <frameset rows="10%, *" border="0">
+          <frame src="title.html" scrolling="no" name="kofi">
+        <frameset cols="20%, *">
+           <frame src="menu.html" scrolling="no">
+           <frame src = "home.php "name="content">
+        </frameset>
+     </frameset>
+
+</html>
+
