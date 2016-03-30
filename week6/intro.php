454e580cd38fdf09071523ba7ba4d8a06622d90c
William Agyapong <willisco@github.com>
2016-03-30 19:10:40 +0100

    Included week6 tutorial files

---
+++ week6/intro.php
@@ -0,0 +1,36 @@
+<?php
+
+echo " hello world! this is my first php program";
+echo "<br>";
+print("I am printing to  the screen. I can't believe it!");
+echo "<p>";
+
+ echo  "this is the content of the paragraph<br>
+        It's really fun working with PHP.<br>
+        See how simple it integrate well with html<br>
+        End of paragraph";
+
+
+ //this is a one line commenting in php";
+
+ /*
+ use this style for multiple lines or block commenting
+ all comments can go here
+ all comments can go here
+ all comments can go here
+ all comments can go here 
+ all comments can go here
+ */
+echo "</p>";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<!-- dighfirfjg
+ortj -->
+</body>
+</html>
